66462c231011f6418fc246789ce4feed10a74a66
Andrew Gleave <andrew@redrobotstudios.com>
2017-05-09 21:33:25 +0100

    Use dateparser for parsing scraped dates

--- web/whim/core/time.py
+++ web/whim/core/time.py
@@ -1,5 +1,15 @@
 from datetime import datetime, timezone, time
 
+import dateparser
+
 
 def zero_time_with_timezone(date, tz=timezone.utc):
     return datetime.combine(date, time(tzinfo=tz))
+
+
+def attempt_parse_date(val):
+    parsed_date = dateparser.parse(val, languages=['en'])
+    if parsed_date is None:
+        # try other strategies?
+        pass
+    return parsed_date
